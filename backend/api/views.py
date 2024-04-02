b53a8d192e90214010caaf1a2abcbfc4e91d9d86
yutaro okuda <yutaro.okuda.76@gmail.com>
2024-04-02 08:01:49 +0800

    views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from .models import Task
+from rest_framework import viewsets
+from .serializers import TaskSerializer
 
 # Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
+
+  def get_queryset(self):
+    queryset = Task.objects.all()
+    deleted = self.request.query_params.get('deleted', None)
+    completed = self.request.query_params.get('completed', None)
+
+    if deleted is not None:
+        queryset = queryset.filter(deleted=deleted.lower() == 'true')
+    if completed is not None:
+        queryset = queryset.filter(completed=completed.lower() == 'true')
+
+    return queryset
